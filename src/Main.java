b0156d0e23a5710dc563d223ab56ed5c461da7f7
Tenno641 <ahmedfotoh2003@gmail.com>
2023-06-28 17:44:42 +0300

    init

--- src/Main.java
+++ src/Main.java
@@ -1,179 +1,267 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
-        Machine m = new Machine();
+        amazingNumbers.showRules();
 
         while (true) {
-            System.out.println("Write action (buy, fill, take):");
-            String action = sc.next();
+            System.out.print("Enter a request: ");
+            String[] num = sc.nextLine().split(" ");
 
-            switch (action) {
-                case "buy" -> {
-                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-                    m.buy(sc.next().charAt(0));
-                }
-
-                case "fill" -> {
-                    System.out.println("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:");
-                    m.setAmountOfWater(sc.nextInt());
-
-                    System.out.println("Write how many ml of milk you want to add:");
-                    m.setAmountOfMilk(sc.nextInt());
-
-                    System.out.println("Write how many grams of coffee beans you want to add:");
-                    m.setAmountOfCoffee(sc.nextInt());
-
-                    System.out.println("Write how many disposable cups you want to add:");
-                    m.setCups(sc.nextInt());
-
-                    m.checkingPossibility();
-                }
-
-                case "take" -> m.take();
-
-                case "remaining" -> m.print();
-
-                case "exit" -> {
-                    return;
-                }
-
+            if ("0".equals(num[0])) {
+                System.out.println("Goodbye!");
+                return;
             }
 
+            if (num[0].isEmpty()) {
+                amazingNumbers.showRules();
+                continue;
+            }
+
+            amazingNumbers am;
+            long first;
+            int second;
+
+            try {
+                first = Long.parseLong(num[0]);
+                second = num.length > 1 ? Integer.parseInt(num[1]) : 0;
+                String state1 = num.length > 2 ? num[2] : "";
+                String state2 = num.length > 3 ? num[3] : "";
+
+                if (first < 1) {
+                    System.out.println("The first parameter should be a natural number or zero.");
+                    continue;
+                } else if (second < 1 && num.length > 1) {
+                    System.out.println("The second parameter should be a natural number.");
+                    continue;
+                }
+
+                if (num.length == 1) {
+                    am = new amazingNumbers(first);
+                } else if (num.length == 2) {
+                    am = new amazingNumbers(first, second);
+                } else if (num.length == 3) {
+                    am = new amazingNumbers(first, second, state1);
+                } else if (num.length == 4) {
+                    am = new amazingNumbers(first, second, state1, state2);
+                }
+            } catch (Exception e) {
+                System.out.println("Invalid input. Please enter valid parameters.");
+            }
         }
 
     }
 
 }
 
-class Machine {
+class amazingNumbers {
 
-    private int possibleEspresso;
-    private int possibleLatte;
-    private int possibleCappuccino;
+    private long num;
+    private HashMap<String, Boolean> myMap;
 
-    private int amountOfWater = 400;
-    private int amountOfMilk = 540;
-    private int amountOfCoffee = 120;
-    private int cups = 9;
-    private int money = 550;
+    public amazingNumbers(long num) {
+        initialize(num);
 
-    public void buy(char choice) {
+        show();
+    }
 
-        switch (choice) {
-            case '1' -> {
+    public amazingNumbers(long num, int freq) {
+        while (freq-- > 0) {
+            initialize(num);
 
-            if(isEspressoPossible()) {
-                    System.out.println("I have enough resources, making you coffee!");
-                    makeEspresso();
-                } else {
-                    System.out.println("Sorry, not enough material!");
-                }
+            show2();
 
+            num++;
+
+        }
+    }
+
+    public amazingNumbers(long num, int freq, String prop) {
+        while (freq > 0) {
+            initialize(num);
+            if (!myMap.containsKey(prop.toLowerCase())) {
+                System.out.printf("The property [%s] is wrong.\n", prop);
+                System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
+                break;
             }
 
-            case '2' -> {
+            if (myMap.get(prop.toLowerCase())) {
+                show2();
 
-                if (isLattePossible()) {
-                    System.out.println("I have enough resources, making you coffee!");
-                    makeLatte();
-                } else {
-                    System.out.println("Sorry, not enough material!");
-                }
-
+                freq--;
             }
 
-            case '3' -> {
-                if ((isCappuccinoPossible())) {
-                    System.out.println("I have enough resources, making you coffee!");
-                    makeCappuccino();
-                } else {
-                    System.out.println("Sorry, not enough material!");
-                }
+            num++;
+
+        }
+    }
+
+    public amazingNumbers(long num, int freq, String prop1, String prop2) {
+        while (freq > 0) {
+            initialize(num);
+            if (!myMap.containsKey(prop1.toLowerCase()) && !myMap.containsKey(prop2.toLowerCase())) {
+                System.out.printf("The properties [%s, %s] are wrong.\n", prop1, prop2);
+                System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
+                break;
+            } else if (!myMap.containsKey(prop1.toLowerCase())) {
+                System.out.printf("The property [%s] is wrong.\n", prop1);
+                System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
+                break;
+            } else if (!myMap.containsKey(prop2.toLowerCase())) {
+                System.out.printf("The property [%s] is wrong.\n", prop2);
+                System.out.println("Available properties: [EVEN, ODD, BUZZ, DUCK, PALINDROMIC, GAPFUL, SPY, SQUARE, SUNNY]");
+                break;
             }
 
-            default -> { }
+            String invalid = prop1+prop2;
+            if (invalid.equals("oddeven") || invalid.equals("squaresunny") || invalid.equals("spyduck") || invalid.equals("duckspy") || invalid.equals("sunnysquare") || invalid.equals("evenodd")) {
+                System.out.printf("The request contains mutually exclusive properties: [%s, %s]\n", prop1, prop2);
+                System.out.println("There are no numbers with these properties.");
+                break;
+            }
+
+            if (myMap.get(prop1.toLowerCase()) && myMap.get(prop2.toLowerCase())) {
+                show2();
+
+                freq--;
+            }
+
+            num++;
 
         }
 
     }
 
-    public void checkingPossibility() {
-        possibleEspresso = Math.min(amountOfWater / 250, amountOfCoffee / 16 );
-        possibleLatte = Math.min(amountOfWater / 350, Math.min(amountOfCoffee / 20, amountOfMilk / 75 ));
-        possibleCappuccino = Math.min(amountOfWater / 200, Math.min(amountOfCoffee / 12, amountOfMilk / 100 ));
+    private void initialize(long num) {
+        myMap = new HashMap<>();
+        this.num = num;
+        myMap.put("even", checkingEven());
+        myMap.put("odd", checkingOdd());
+        myMap.put("square", checkingSquare());
+        myMap.put("sunny", checkingSunny());
+        myMap.put("gapful", checkingGapful());
+        myMap.put("duck", checkingDuck());
+        myMap.put("buzz", checkingBuzz());
+        myMap.put("palindromic", checkingPalindromic());
+        myMap.put("spy", checkingSpy());
     }
 
-    private boolean isEspressoPossible() {
-        return 1 >= possibleEspresso;
+    private boolean checkingBuzz() {
+        return num % 7 == 0 || num % 10 == 7;
     }
 
-    private boolean isLattePossible() {
-        return 1 >= possibleLatte;
+    private boolean checkingDuck() {
+        StringBuilder exp = new StringBuilder(String.valueOf(num));
+
+        for (int i = 0; i < exp.length(); i++) {
+            if (exp.charAt(i) == '0') {
+                exp.deleteCharAt(i);
+            } else {
+                break;
+            }
+        }
+
+        return exp.toString().contains("0");
     }
 
-    private boolean isCappuccinoPossible() {
-        return 1 >= possibleCappuccino;
+    private boolean checkingPalindromic() {
+        String exp = String.valueOf(num);
+
+        return exp.equals(new StringBuilder(exp).reverse().toString());
     }
 
-    private void makeEspresso() {
-        this.amountOfWater -= 250;
-        this.amountOfCoffee -= 16;
-        this.cups -= 1;
-        this.money += 4;
+    private boolean checkingGapful() {
+        String exp = String.valueOf(num);
+
+        if (exp.length() <= 2) {
+            return false;
+        }
+
+        return Long.parseLong(exp) % Long.parseLong(String.format("%c%c", exp.charAt(0), exp.charAt(exp.length() - 1))) == 0;
+
     }
 
-    private void makeLatte() {
-        this.amountOfWater -= 350;
-        this.amountOfMilk -= 75;
-        this.amountOfCoffee -= 20;
-        this.cups -= 1;
-        this.money += 7;
+    private boolean checkingSpy() {
+        int sum = 0;
+        int product = 1;
+
+        String[] arr = String.valueOf(num).split("");
+
+        for (String s : arr) {
+            sum += Integer.parseInt(s);
+            product *= Integer.parseInt(s);
+        }
+
+        return sum == product;
+
     }
 
-    private void makeCappuccino() {
-        this.amountOfWater -= 200;
-        this.amountOfMilk -= 100;
-        this.amountOfCoffee -= 12;
-        this.cups -= 1;
-        this.money += 6;
+    private boolean checkingSunny() {
+        return Math.sqrt(num + 1) % 1 == 0;
     }
 
-    public void setAmountOfWater(int amountOfWater) {
-        this.amountOfWater += amountOfWater;
+    private boolean checkingSquare() {
+        return Math.sqrt(num) % 1 == 0;
     }
 
-    public void setAmountOfMilk(int amountOfMilk) {
-        this.amountOfMilk += amountOfMilk;
+    private boolean checkingEven() {
+        return num % 2 == 0;
     }
 
-    public void setAmountOfCoffee(int amountOfCoffee) {
-        this.amountOfCoffee += amountOfCoffee;
+    private boolean checkingOdd() {
+        return num % 2 == 1;
     }
 
-    public void setCups(int cups) {
-        this.cups += cups;
+    public void show() {
+        System.out.printf("""
+        Properties of %d
+                buzz: %b
+                duck: %b
+         palindromic: %b
+                 spy: %b
+              gapful: %b
+                even: %b
+                 odd: %b
+               sunny: %b
+              square: %b
+                 """, num, myMap.get("buzz"), myMap.get("duck"), myMap.get("palindromic"), myMap.get("spy"), myMap.get("gapful"), myMap.get("even"), myMap.get("odd"), myMap.get("sunny"), myMap.get("square"));
     }
 
-    public void take() {
-        System.out.printf("I gave you $%d\n", money);
-        setMoney();
+    private void show2() {
+        StringBuilder res = new StringBuilder(String.format("%d is", num));
+
+        if (myMap.get("buzz")) { res.append(" buzz,"); }
+        if (myMap.get("duck")) { res.append(" duck,"); }
+        if (myMap.get("palindromic")) { res.append(" palindromic,"); }
+        if (myMap.get("spy")) { res.append(" spy,"); }
+        if (myMap.get("sunny")) { res.append(" sunny,"); }
+        if (myMap.get("square")) { res.append(" square,"); }
+        if (myMap.get("gapful")) { res.append(" gapful,"); }
+        if (myMap.get("even")) { res.append(" even,"); }
+        if (myMap.get("odd")) { res.append(" odd,"); }
+
+        res.deleteCharAt(res.length() - 1);
+        System.out.println(res);
     }
 
-    private void setMoney() {
-        this.money = 0;
-    }
-
-    public void print() {
-        System.out.println("The coffee machine has:");
-        System.out.printf("%d ml of water\n", amountOfWater);
-        System.out.printf("%d ml of milk\n", amountOfMilk);
-        System.out.printf("%d g of coffee beans\n", amountOfCoffee);
-        System.out.printf("%d disposable cups\n", cups);
-        System.out.printf("$%d of money\n", money);
+    public static void showRules() {
+        System.out.println("""
+                Welcome to Amazing Numbers!
+                                
+                Supported requests:
+                - enter a natural number to know its properties;
+                - enter two natural numbers to obtain the properties of the list:
+                  * the first parameter represents a starting number;
+                  * the second parameters show how many consecutive numbers are to be processed;
+                - two natural numbers and two properties to search for;
+                - separate the parameters with one space;
+                - enter 0 to exit.
+                """);
     }
 
 }
---
+++ src/hehe/hoho.java
@@ -0,0 +1,16 @@
+package hehe;
+
+import java.util.Scanner;
+
+public class hoho {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String text = "4780772187520985735";
+        long num = sc.nextLong();
+        System.out.println(Long.parseLong(text));
+        System.out.println(num);
+    }
+
+}
